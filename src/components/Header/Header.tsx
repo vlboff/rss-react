508bf16ffc1e1dc02132df6c74924fc52734c50f
Uladzimir <vl.barbikov@gmail.com>
2023-03-14 23:44:59 +0300

    feat: add header buttons hendler

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,33 @@
+import { Component, ReactNode } from 'react';
 import HeaderButtom from '../UI/HeaderButtom/HeaderButtom';
 
-function Header() {
-  return (
-    <div>
-      <HeaderButtom name={'Home'} link={'/'} />
-      <HeaderButtom name={'About us'} link={'/about'} />
-    </div>
-  );
+class Header extends Component<object, { activeMode: string }> {
+  state = {
+    activeMode: '',
+  };
+
+  addActive = (name: string) => {
+    this.setState({ activeMode: name });
+  };
+
+  render(): ReactNode {
+    return (
+      <div>
+        <HeaderButtom
+          name={'Home'}
+          link={'/'}
+          onClick={() => this.addActive('Home')}
+          activeMode={this.state.activeMode}
+        />
+        <HeaderButtom
+          name={'About us'}
+          link={'/about'}
+          onClick={() => this.addActive('About us')}
+          activeMode={this.state.activeMode}
+        />
+      </div>
+    );
+  }
 }
 
 export default Header;
--- src/components/UI/HeaderButtom/HeaderButtom.tsx
+++ src/components/UI/HeaderButtom/HeaderButtom.tsx
@@ -1,11 +1,26 @@
+import { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-function HeaderButtom(props: { name: string; link: string }) {
-  return (
-    <div>
-      <Link to={props.link}>{props.name}</Link>
-    </div>
-  );
+class HeaderButtom extends Component<{
+  name: string;
+  link: string;
+  onClick: () => void;
+  activeMode: string;
+}> {
+  constructor(props: { name: string; link: string; onClick: () => void; activeMode: string }) {
+    super(props);
+  }
+
+  render() {
+    const { name, link, onClick, activeMode } = this.props;
+    return (
+      <div className={`nav-links ${activeMode === name ? 'active' : ''}`}>
+        <Link to={link} onClick={onClick}>
+          {name}
+        </Link>
+      </div>
+    );
+  }
 }
 
 export default HeaderButtom;
