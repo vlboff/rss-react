fc1c1021b1243c10922a411b245f7ad124ec1a7c
Uladzimir <vl.barbikov@gmail.com>
2023-04-28 18:18:20 +0300

    feat: set cypress coverage config

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,11 +3,24 @@ import coverage from '@cypress/code-coverage/task';
 
 export default defineConfig({
   video: false,
+  env: {
+    codeCoverage: {
+      exclude: 'cypress/**/*.*',
+    },
+  },
+
   e2e: {
     baseUrl: 'http://localhost:3001',
     setupNodeEvents(on, config) {
       coverage(on, config);
       return config;
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
     },
   },
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+/// <reference types="cypress" />
 
 import { defineConfig } from 'vite';
 import { configDefaults } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import istanbul from 'vite-plugin-istanbul';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [
+    svgr(),
+    react(),
+    istanbul({
+      cypress: true,
+      requireEnv: false,
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
