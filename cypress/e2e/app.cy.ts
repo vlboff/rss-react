59ae854e066ac753de5159a466fdad5a55d3bb87
Uladzimir <vl.barbikov@gmail.com>
2023-04-29 16:38:46 +0300

    feat: add e2e tests

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,7 +1,47 @@
 describe('app e2e', () => {
-  it('should have input', () => {
+  afterEach(() => {
+    cy.window().trigger('unload');
+  });
+
+  it('search input', () => {
     cy.visit('/');
 
     cy.get('input').should('have.value', '');
+    cy.get('input').type('Avatar{enter}');
+    cy.get('input').should('have.value', 'Avatar');
+  });
+
+  it('navigation', () => {
+    cy.visit('/');
+
+    cy.get('h3').contains('About us').click();
+    cy.url().should('include', '/about');
+    cy.get('h3').contains('Forms').click();
+    cy.url().should('include', '/forms');
+  });
+
+  it('404', () => {
+    cy.visit('/404');
+    cy.contains('Page not found');
+  });
+
+  it('form invalid', () => {
+    cy.visit('/forms');
+
+    cy.get('input[type="submit"]').click();
+    cy.get('.invalid-form-text').contains('*add title');
+  });
+
+  it('form valid', () => {
+    cy.visit('/forms');
+
+    cy.get('input[type="text"]').type('Avatar').should('have.value', 'Avatar');
+    cy.get('.input-poster').selectFile('cypress/fixtures/avatar_poster.jpg');
+    cy.get('select').select(1).should('have.value', 'Action');
+    cy.get('input[type="date"]').type('1990-08-20').should('have.value', '1990-08-20');
+    cy.get('input[type="radio"]').first().check().should('be.checked').and('have.value', 'Rumored');
+    cy.get('input[type="checkbox"]').check();
+    cy.get('input[type="submit"]').click();
+    cy.get('h3').contains('Avatar');
   });
 });
